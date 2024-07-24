b5d3e53166558468ae91f238670e83bc3fbf4e28
KKruglov27 <diluc.ragnvindr.04.30.98@gmail.com>
2024-07-24 04:30:54 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,19 @@
+question_1 = 'Ваше любимое увлечение?'
+my_strings = (input(question_1))
+
+Len_function = (len(question_1))
+upper_function = (question_1.upper())
+lower_function = (question_1.lower())
+replace_function = (question_1.replace(' ',''))
+slice_function_first_letter = (question_1[0])
+slice_function_last_letter = (question_1[-1])
+
+if (my_strings == 'task1'):
+    print(Len_function)
+elif (my_strings == 'task2'):
+    print((upper_function), (lower_function), (replace_function), (slice_function_first_letter), (slice_function_last_letter))
+
+
+
+
+

ff9fb9892a02dada64f1cdf066724a3b1b9994aa
KKruglov27 <diluc.ragnvindr.04.30.98@gmail.com>
2024-07-24 19:58:03 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_strings = input('Ваше любимое увлечение?')
+Len_function = len(my_strings)
+upper_function = my_strings.upper()
+lower_function = my_strings.lower()
+replace_function = my_strings.replace(' ','')
+slice_function_first_letter = my_strings[0]
+slice_function_last_letter = my_strings[-1]
+print(Len_function)
+print(upper_function, lower_function, replace_function, slice_function_first_letter, slice_function_last_letter)

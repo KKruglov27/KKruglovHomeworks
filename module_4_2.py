ae6eca5abbc8c7a49d5971043909fd3f1520b3fc
KKruglov27 <diluc.ragnvindr.04.30.98@gmail.com>
2024-08-18 11:08:43 +0300

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,47 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+test_function()
+
+print()
+
+
+def test_function(prev_type):
+    if isinstance(prev_type, str):
+        print(prev_type)
+    else:
+        print("Тип этой переменной в области видимости 'test_function', уже не STR, он был изменен")
+    print()
+    def inner_function(prev_type):
+        new_type = {"Я value1 в области видимости функции": "'test_function', по моему я все так же строка"}
+        prev_type = new_type
+        if isinstance(prev_type, str):
+            print(prev_type)
+        else:
+            print("Тип этой переменной в подфункции 'inner_functions' уже не STR, он был изменен в данной подфункции.")
+    inner_function(prev_type)
+
+
+value1 = "Я в области видимости функции test_function, пока что я строка"
+test_function(value1)
+
+
+#________________________________________________________________
+
+
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+# inner_function()
+
+# inner_function()
+#    ^^^^^^^^^^^^^^
+# NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+
+# Вызов подфункции в глобальном пространстве имен может быть осуществлен только
+# с помощью родительской функции.

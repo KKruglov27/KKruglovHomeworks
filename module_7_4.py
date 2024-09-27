1e7eb8910ec5b76ddc78389b3399eb26325196cc
KKruglov27 <diluc.ragnvindr.04.30.98@gmail.com>
2024-09-27 12:20:39 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,30 @@
+
+team1_num = 5
+team2_num = 6
+score_1 = 53
+score_2 = 52
+team1_time = 2230.34003
+team2_time = 2230.34002
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time) / (score_1 + score_2)
+challenge_result = 'Победа команды'
+team1_name = 'Мастера кода'
+team2_name = 'Волшебники данных'
+
+
+print('В команде "Мастера кода" участников: %d!' % team1_num)
+print('Итого сегодня в командах участников: %d и %d:' % (team1_num, team2_num))
+
+print('Команда {team2} решила задач {score}!'.format(team2=team2_name, score=score_2))
+print('{team2} решили задачи за {time} c!'.format(team2=team2_name, time=team2_time))
+
+print(f'Команды решили {score_1} и {score_2} задач')
+
+if score_1 == score_2 and team1_time == team2_time:
+    print(f'Результат битвы: Ничья!')
+else:
+    (print(f'Результат битвы: {challenge_result} {team2_name if score_2 > score_1 else team1_name}!' or
+f'Результат битвы: {challenge_result} {team2_name if score_2 == score_1 and team1_time > team2_time else team1_name}!')
+and f'Результат битвы: {challenge_result} {team1_name if score_2 == score_1 and team1_time < team2_time else team2_name}!')
+
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {round(time_avg, 2)} секунды на задачу!')
